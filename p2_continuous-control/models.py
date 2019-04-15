211e5777ebc75ef02a1811be68a5305941ca06f4
postBG <profile2697@gmail.com>
2019-04-16 01:23:10 +0900

    add actor model

---
+++ p2_continuous-control/models.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SimpleActorModel(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, fc1_units=256, fc2_units=128):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super().__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+
+    def forward(self, state):
+        """Build a network that maps state -> action probabilities."""
+        h = F.relu(self.fc1(state))
+        h = F.relu(self.fc2(h))
+        return self.fc3(h)
