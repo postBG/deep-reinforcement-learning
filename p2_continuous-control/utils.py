b771d868bc6caf75a5baebe0165af45e4b41a69f
postBG <profile2697@gmail.com>
2019-04-16 02:21:58 +0900

    add some utils

---
+++ p2_continuous-control/utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def calculate_future_rewards(rewards, gamma):
+    """rewards' shape is [max_episode_length * batch_size]"""
+    discounts = gamma ** np.arange(len(rewards))
+    discounted_rewards = np.asarray(rewards) * discounts[:, np.newaxis]
+    future_rewards = discounted_rewards[::-1].cumsum(axis=0)[::-1]
+    return future_rewards
+
+
+def normalize_rewards(rewards):
+    means = np.mean(rewards, axis=1)
+    stds = np.std(rewards, axis=1) + 1e-10
+    return (rewards - means[:, np.newaxis]) / stds[:, np.newaxis]
