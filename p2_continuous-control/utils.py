8d7fd0fe64be1c6c4f265b91b0300dce7b624ce4
postBG <profile2697@gmail.com>
2019-04-19 02:43:29 +0900

    feat: add utils

--- p2_continuous-control/utils.py
+++ p2_continuous-control/utils.py
@@ -1,4 +1,9 @@
+import math
+
 import numpy as np
+import torch
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 def calculate_future_rewards(rewards, gamma):
@@ -14,3 +19,23 @@ def normalize_rewards(rewards):
     means = np.mean(rewards, axis=1)
     stds = np.std(rewards, axis=1) + 1e-10
     return (rewards - means[:, np.newaxis]) / stds[:, np.newaxis]
+
+
+def log_density(x, mu, std, logstd):
+    var = std.pow(2)
+    log_dens = -(x - mu).pow(2) / (2 * var) - 0.5 * math.log(2 * math.pi) - logstd
+    return log_dens.sum(1, keepdim=True)
+
+
+def to_tensor_long(numpy_array):
+    return torch.LongTensor(numpy_array).to(device)
+
+
+def to_tensor(numpy_array):
+    return torch.Tensor(numpy_array).to(device)
+
+
+def sample_actions(mu, std):
+    actions = torch.normal(mu, std)
+    actions = actions.cpu().data.numpy()
+    return actions
