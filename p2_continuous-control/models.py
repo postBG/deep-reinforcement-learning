8403dc1326fcb1e5b48c1e9a72759f71c158e068
postBG <profile2697@gmail.com>
2019-04-19 02:32:33 +0900

    feat: add critic and change actor to produce mu, std

--- p2_continuous-control/models.py
+++ p2_continuous-control/models.py
@@ -3,7 +3,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class SimpleActorModel(nn.Module):
+def swish(x):
+    return x * F.sigmoid(x)
+
+
+class Actor(nn.Module):
     """Actor (Policy) Model."""
 
     def __init__(self, state_size, action_size, seed, fc1_units=256, fc2_units=128):
@@ -20,8 +24,35 @@ class SimpleActorModel(nn.Module):
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
 
+        self.fc3.weight.data.mul_(0.1)
+        self.fc3.bias.data.mul_(0.0)
+
     def forward(self, state):
-        """Build a network that maps state -> action probabilities."""
-        h = F.relu(self.fc1(state))
-        h = F.relu(self.fc2(h))
-        return F.tanh(self.fc3(h))  # all actions between -1 and 1
+        """Build a network that maps state -> actions mu."""
+        h = swish(self.fc1(state))
+        h = swish(self.fc2(h))
+        mu = self.fc3(h)
+        logstd = torch.zeros_like(mu)
+        std = torch.exp(logstd)
+
+        return mu, std, logstd
+
+
+class Critic(nn.Module):
+    """Critic Model that produces state values"""
+
+    def __init__(self, state_size, seed, fc1_units=256, fc2_units=128):
+        super().__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, 1)
+
+        self.fc3.weight.data.mul_(0.1)
+        self.fc3.bias.data.mul_(0.0)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        v = self.fc3(x)
+        return v
