09ea519235059617305c86e1707f7b44d1df8f64
postBG <profile2697@gmail.com>
2019-04-20 18:57:59 +0900

    feat: add calculate generalized advantages estimations

--- p2_continuous-control/utils.py
+++ p2_continuous-control/utils.py
@@ -1,24 +1,22 @@
 import math
+from itertools import accumulate as accum
 
-import numpy as np
 import torch
+import numpy as np
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-def calculate_future_rewards(rewards, gamma):
-    """rewards is list of episodes where length of list is max_episode_length.
-    And each elements is rewards of each batches. So, shape of the rewards becomes [max_episode_length, batch_size]"""
-    discounts = gamma ** np.arange(len(rewards))
-    discounted_rewards = np.asarray(rewards) * discounts[:, np.newaxis]
-    future_rewards = discounted_rewards[::-1].cumsum(axis=0)[::-1]
-    return future_rewards
+def accumulate(x, discount_rate=1):
+    reversed_x = x[::-1]
+    accumulated_x = np.asarray(list(accum(reversed_x, lambda prev, curr: prev * discount_rate + curr)))[::-1]
+    return accumulated_x
 
 
-def normalize_rewards(rewards):
-    means = np.mean(rewards, axis=1)
-    stds = np.std(rewards, axis=1) + 1e-10
-    return (rewards - means[:, np.newaxis]) / stds[:, np.newaxis]
+def batch_normalize(x):
+    means = np.mean(x, axis=1)
+    stds = np.std(x, axis=1) + 1e-10
+    return (x - means[:, np.newaxis]) / stds[:, np.newaxis]
 
 
 def log_density(x, mu, std, logstd):
